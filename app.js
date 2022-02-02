3a3b9fadfb0712f7d15c39385ab9055b1ae00503
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-02-02 11:28:11 -0300

    Create EJS template and get kind of day

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (request, response) {
-    response.send("Hello");
+    var today = new Date();
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    var day = today.toLocaleDateString('pt-BR', options)
+
+    response.render('list', { kindOfDay: day });
 });
 
 
 
 app.listen(3000, function () {
     console.log("✔ Server is running on port 3000");
-})
+});
