aa1bdd6169f34082a84c46560fbb98964bdd06a0
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-02-10 13:27:21 -0300

    Create new custom list

--- app.js
+++ app.js
@@ -30,11 +30,20 @@ const hint2 = new Item({
     name: '<-- Hit this to delete an item.'
 });
 
+const defaultItems = [welcome, hint1, hint2];
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+});
+
+const List = mongoose.model('List', listSchema);
+
 app.get('/', function (request, response) {
     const day = date.getDate();
     Item.find(function (err, documents) {
         if (documents.length === 0) { /// if there is no documents on database
-            Item.insertMany([welcome, hint1, hint2], function (err) {
+            Item.insertMany(defaultItems, function (err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -46,23 +55,33 @@ app.get('/', function (request, response) {
     });
 });
 
+app.get('/:listTitle', function (request, response) {
+    const newListTitle = request.params.listTitle;
 
-app.get('/work', function (request, response) {
-    response.render('list', { listTitle: 'Work list', allItems: workItems })
+    List.findOne({ name: newListTitle }, function (err, result) {
+        if (!err) {
+            if (!result) { // doesn't exists, so create a new one
+                const list = new List({
+                    name: newListTitle,
+                    items: defaultItems
+                });
+
+                list.save();
+                response.redirect('/' + newListTitle)
+            } else { // exists
+                // console.log('There is already a list with that name.');
+                response.render('list', { listTitle: request.params.listTitle + " list", allItems: result.items })
+            }
+        } else {
+            console.log("Unable to find list.");
+        }
+    });
 });
 
 app.post('/', function (request, response) {
     const newItem = new Item({
         name: request.body.todoItem
     });
-
-    // if (request.body.listSubject === 'Work') {
-    //     workItems.push(item);
-    //     response.redirect('/work')
-    // } else {
-    //     items.push(item);
-    //     response.redirect('/');
-    // }
 
     newItem.save();
     response.redirect('/');
